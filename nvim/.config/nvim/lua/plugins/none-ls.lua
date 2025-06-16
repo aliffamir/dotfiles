5f7cb4aef68bcaf05148d64d7092db2004485512
Aliff Amir <aliff6406@gmail.com>
2025-06-16 18:55:06 +0100

    add clang_format formatter and alpha-nvim

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,11 +1,15 @@
 return {
   "nvimtools/none-ls.nvim",
   config = function()
+    -- TODO: setup format on save see (https://www.youtube.com/watch?v=lsFoZIg-oDs&t=129s)
+    -- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
     local null_ls = require("null-ls")
+
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.clang_format,
         null_ls.builtins.diagnostics.eslint_d,
       },
     })
