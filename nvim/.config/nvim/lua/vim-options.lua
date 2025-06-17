15d5a845007083c47217b5b0012c6302f3287856
Aliff Amir <aliff6406@gmail.com>
2025-06-17 16:04:51 +0100

    add keymaps for copy/paste to clipboard and add floatterm

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -10,4 +10,16 @@ vim.opt.fillchars = { eob = " " }
 
 vim.g.mapleader = " "
 
-vim.keymap.set('t', 'jk', [[<C-\><C-n>]])
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { noremap = true, silent = true })
+
+-- Copy to clipboard
+vim.keymap.set("v", "<leader>y", '"+y', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>Y", '"+yg_', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>y", '"+y', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>yy", '"+yy', { noremap = true, silent = true })
+
+-- Paste from clipboard
+vim.keymap.set("n", "<leader>p", '"+p', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>P", '"+P', { noremap = true, silent = true })
+vim.keymap.set("v", "<leader>p", '"+p', { noremap = true, silent = true })
+vim.keymap.set("v", "<leader>P", '"+P', { noremap = true, silent = true })
