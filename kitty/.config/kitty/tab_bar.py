1d41ea7d84b4a1f55ffce4aa72be20aad3e7462e
Aliff Amir <aliff6406@gmail.com>
2025-06-21 14:05:51 +0100

    update custom tab bar

--- kitty/.config/kitty/tab_bar.py
+++ kitty/.config/kitty/tab_bar.py
@@ -1,38 +1,124 @@
-import math
-import os
-from pathlib import Path
+from datetime import datetime
 from kitty.boss import get_boss
-from kitty.fast_data_types import Screen, get_options, Color
+from kitty.fast_data_types import Screen, add_timer
+from kitty.rgb import to_color
 from kitty.tab_bar import (
     DrawData,
     ExtraData,
+    Formatter,
     TabBarData,
     as_rgb,
-    draw_title
+    draw_attributed_string,
+    draw_tab_with_powerline,
 )
 
-def createLogDir():
-    xdg_state_home = os.getenv("XDG_STATE_HOME", Path.home() / ".local" / "state")
-    
-    # Define the log directory path
-    log_dir = Path(xdg_state_home) / "kitty"
-    
-    # Create the directory (if it doesn't exist)
-    log_dir.mkdir(parents=True, exist_ok=True)
+LEFT_HALF_CIRCLE = ""
+TERMINAL_ICON = " "
+REFRESH_TIME = 1
 
-    return log_dir
 
-opts = get_options()
+def _get_active_process_name_cell() -> dict:
+    cell = {"icon": TERMINAL_ICON, "icon_bg_color": "#a8e4a4", "text": ""}
+    boss = get_boss()
 
-lavender = as_rgb(int("B4BEFE", 16))
-surface1 = as_rgb(int("45475A", 16))
-base = as_rgb(int("1E1E2E", 16))
-window_icon = ""
-layout_icon = ""
-active_tab_layout_name = ""
-active_tab_num_windows = 1
-left_status_length = 0
-log_dir = createLogDir()
+    # Error 1: No boss instance found.
+    if not boss:
+        cell["text"] = "Err 1"
+        return cell
+
+    active_window = boss.active_window
+    # Error 2: No active window found
+    if not active_window:
+        cell["text"] = "Err 2"
+        return cell
+    # Error 3: No process is associated with the active window.
+    if not active_window.child:
+        cell["text"] = "Err 3"
+        return cell
+
+    foreground_processes = active_window.child.foreground_processes
+    # Error 4: No foreground process found.
+    if not foreground_processes or not foreground_processes[0]["cmdline"]:
+        cell["text"] = "Err 4"
+        return cell
+    long_process_name = foreground_processes[0]["cmdline"][0]
+    cell["text"] = long_process_name.rsplit("/", 1)[-1]
+
+    return cell
+
+
+def _get_datetime_cell() -> dict:
+    now = datetime.now().strftime("%d-%m-%Y %H:%M")
+    return {"icon": CALENDAR_CLOCK_ICON, "icon_bg_color": "#90b4fc", "text": now}
+
+
+def _get_battery_cell() -> dict:
+    cell = {"icon": "", "icon_bg_color": "#f9e2af", "text": ""}
+
+    try:
+        with open("/sys/class/power_supply/BAT0/status", "r") as f:
+            status = f.read()
+        with open("/sys/class/power_supply/BAT0/capacity", "r") as f:
+            percent = int(f.read())
+
+        if status == "Charging\n":
+            cell["icon"] = CHARGING_ICON
+        else:
+            cell["icon"] = UNPLUGGED_ICONS[
+                min(UNPLUGGED_ICONS.keys(), key=lambda x: abs(percent - x))
+            ]
+        cell["text"] = str(percent) + "%"
+
+    except FileNotFoundError:
+        cell["text"] = "Err"
+
+    return cell
+
+
+def _create_cells() -> list[dict]:
+    # return [_get_battery_cell(), _get_active_process_name_cell(), _get_datetime_cell()]
+    return [_get_active_process_name_cell()]
+
+
+def _draw_right_status(screen: Screen, is_last: bool, draw_data: DrawData) -> int:
+    if not is_last:
+        return 0
+    draw_attributed_string(Formatter.reset, screen)
+
+    cells = _create_cells()
+    right_status_length = 0
+    for c in cells:
+        right_status_length += 3 + len(c["icon"]) + len(c["text"])
+
+    screen.cursor.x = screen.columns - right_status_length
+
+    default_bg = as_rgb(int(draw_data.default_bg))
+    tab_fg = as_rgb(int(draw_data.inactive_fg))
+
+    screen.cursor.bg = default_bg
+    for c in cells:
+        icon_bg_color = as_rgb(int(to_color(c["icon_bg_color"])))
+        screen.cursor.fg = icon_bg_color
+        screen.draw(LEFT_HALF_CIRCLE)
+
+        screen.cursor.bg = icon_bg_color
+        screen.cursor.fg = 1
+        screen.draw(c["icon"])
+
+        screen.cursor.bg = as_rgb(int(to_color("#383444")))
+        screen.cursor.fg = tab_fg
+        screen.draw(f" {c['text']} ")
+
+    return screen.cursor.x
+
+
+def _redraw_tab_bar(_) -> None:
+    tm = get_boss().active_tab_manager
+    if tm is not None:
+        tm.mark_tab_bar_dirty()
+
+
+timer_id = None
 
 
 def draw_tab(
@@ -45,136 +131,11 @@ def draw_tab(
     is_last: bool,
     extra_data: ExtraData,
 ) -> int:
-
-    
-    # Open the file in write mode
-    global base
-    global active_tab_layout_name
-    global active_tab_num_windows
-
-    try:
-        # active_tab_idx = get_boss().active_tab_manager.active_tab_idx
-        # curr_tab_id = tab.tab_id
-        output = get_boss().call_remote_control(None, ('get-colors', f'--match=recent:0'))
-
-        lines = output.split('\n')
-        background_value = None
-        
-        for line in lines:
-            if line.startswith('background'):
-                background_value = line.split()[1]
-                break
-
-        
-        base = int(background_value[1:], 16)
-        r,g,b = extract_rgb(base)
-        base_color = Color(r,g,b)
-
-        new_draw_data = draw_data._replace(inactive_bg=base_color)
-
-        draw_tab_with_separator(new_draw_data, screen, tab, before, max_title_length, index, is_last, extra_data, as_rgb(base))
-    except Exception as e:
-        with open(log_dir / "tab_bar.log", "a") as f:
-            f.write(f"Error: {e}\n")
-
-
-
-    return screen.cursor.x
-
-def draw_tab_with_separator(
-    draw_data: DrawData, screen: Screen, tab: TabBarData,
-    before: int, max_tab_length: int, index: int, is_last: bool,
-    extra_data: ExtraData,
-    background: int
-) -> int:
-    screen.cursor.bg = background
-    screen.cursor.fg = as_rgb(draw_data.active_fg.rgb)
-    screen.cursor.bold = screen.cursor.italic = False
-
-    if index==1:
-        screen.draw(draw_data.sep)
-
-
-    if tab.is_active:
-        screen.cursor.bg = background
-        screen.cursor.fg = as_rgb(draw_data.active_bg.rgb)
-        screen.draw('')
-        screen.cursor.bg = as_rgb(draw_data.active_bg.rgb)
-        screen.cursor.fg = as_rgb(draw_data.active_fg.rgb)
-    else:
-        screen.cursor.bg = as_rgb(draw_data.inactive_bg.rgb)
-        screen.cursor.fg = as_rgb(draw_data.inactive_fg.rgb)
-
-
-    draw_title(draw_data, screen, tab, index, max_tab_length)
-
-    if tab.is_active:
-        screen.cursor.bg = background
-        screen.cursor.fg = as_rgb(draw_data.active_bg.rgb)
-        screen.draw('')
-        screen.cursor.bg = background
-        screen.cursor.fg = as_rgb(draw_data.active_fg.rgb)
-    else:
-        screen.cursor.bg = background
-        screen.cursor.fg = as_rgb(draw_data.inactive_fg.rgb)
-
-
-
-    if not is_last:
-        screen.draw(draw_data.sep)
-
-    if is_last:
-        remaining_size = screen.columns - screen.cursor.x
-        cwd = truncate_str(get_cwd() + draw_data.sep , remaining_size) 
-
-        screen.cursor.bg = background
-        screen.cursor.fg = as_rgb(draw_data.inactive_fg.rgb)
-        screen.cursor.bold = screen.cursor.italic = False
-        screen.draw(' ' * (remaining_size - len(cwd)))
-        screen.draw(cwd)
-
-    end = screen.cursor.x
-    return end
-
-def truncate_str(input_str, max_length):
-    if len(input_str) > max_length:
-        half = max_length // 2
-        return input_str[:half] + "…" + input_str[-half:]
-    else:
-        return input_str
-
-def get_cwd():
-    cwd = ""
-    tab_manager = get_boss().active_tab_manager
-    if tab_manager is not None:
-        window = tab_manager.active_window
-        if window is not None:
-            cwd = window.cwd_of_child
-
-    cwd_parts = list(Path(cwd).parts)
-
-    if len(cwd_parts) > 1:
-        if cwd_parts[1] == "home" or str(Path(*cwd_parts[:3])) == os.getenv("HOME") and len(cwd_parts) > 3:
-            # replace /home/{{username}}
-            cwd_parts = ["~"] + cwd_parts[3:]
-            if len(cwd_parts) > 1:
-                cwd_parts[0] = "~/"
-        else:
-            cwd_parts[0] = "/"
-    else:
-        cwd_parts[0] = "/"
-
-    max_length = 10
-    cwd = cwd_parts[0] + "/".join(
-        [
-            s if len(s) <= max_length else truncate_str(s, max_length)
-            for s in cwd_parts[1:]
-        ]
+    global timer_id
+    if timer_id is None:
+        timer_id = add_timer(_redraw_tab_bar, REFRESH_TIME, True)
+    draw_tab_with_powerline(
+        draw_data, screen, tab, before, max_title_length, index, is_last, extra_data
     )
-    return cwd
-
-def extract_rgb(hex_color: int):
-    r = (hex_color >> 16) & 0xFF  # Extracts the red component
-    g = (hex_color >> 8) & 0xFF   # Extracts the green component
-    b = hex_color & 0xFF          # Extracts the blue component
-    return r, g, b
+    _draw_right_status(screen, is_last, draw_data)
+    return screen.cursor.x
