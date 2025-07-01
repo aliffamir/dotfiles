fde2c87c0e0b3a3ca1afb3e51fe30f6dc6fce416
Aliff Amir <aliff6406@gmail.com>
2025-07-01 23:15:31 +0100

    add hcl and terraform support

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,13 @@ return {
     local config = require("nvim-treesitter.configs")
     config.setup({
       auto_install = true,
-      ensure_installed = { "comment", "markdown", "markdown_inline" },
+      ensure_installed = {
+        "comment",
+        "markdown",
+        "markdown_inline",
+        "hcl",
+        "terraform",
+      },
       highlight = { enable = true },
       indent = { enable = true },
     })
