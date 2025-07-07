613f8a57a90ed95867a352c1b094d2a3f9736e78
Aliff Amir <aliff6406@gmail.com>
2025-07-07 23:04:08 +0100

    fix roslyn lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,54 +1,54 @@
 return {
-    {
-        "mason-org/mason.nvim",
-        lazy = false,
-        config = function()
-            require("mason").setup({
-                registries = {
-                    "github:mason-org/mason-registry",
-                    "github:Crashdummyy/mason-registry",
-                },
-            })
-        end,
-    },
-    {
-        "mason-org/mason-lspconfig.nvim",
-        lazy = false,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        lazy = false,
-        config = function(event)
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            local lspconfig = require("lspconfig")
+	{
+		"mason-org/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup({
+				registries = {
+					"github:mason-org/mason-registry",
+					"github:Crashdummyy/mason-registry",
+				},
+			})
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		lazy = false,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function(event)
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
 
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.ts_ls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.clangd.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.cmake.setup({
-                capabilities = capabilities,
-                filetypes = { "cmake", "CMakeLists.txt" },
-            })
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.cmake.setup({
+				capabilities = capabilities,
+				filetypes = { "cmake", "CMakeLists.txt" },
+			})
 
-            local opts = { buffer = event.buf, remap = false }
+			local opts = { buffer = event.buf, remap = false }
 
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-            vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-            vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-            vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-            vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-            vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-            vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-            vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-            vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-        end,
-    },
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+			vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+			vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+			vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+			vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+			vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+			vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
+			vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+			vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+		end,
+	},
 }
--- .config/nvim/lua/plugins/roslyn.lua
+++ .config/nvim/lua/plugins/roslyn.lua
@@ -1,24 +1,9 @@
 return {
-  "seblyng/roslyn.nvim",
-  ft = "cs",
-  ---@module 'roslyn.config'
-  ---@type RoslynNvimConfig
-  opts = {
-    -- your configuration comes here; leave empty for default settings
-  },
-  config = function()
-    local lspconfig = require("lspconfig")
-
-    lspconfig.roslyn.setup({
-      settings = {
-        ["csharp|inlay_hints"] = {
-          csharp_enable_inlay_hints_for_implicit_object_creation = true,
-          csharp_enable_inlay_hints_for_implicit_variable_types = true,
-        },
-        ["csharp|code_lens"] = {
-          dotnet_enable_references_code_lens = true,
-        },
-      },
-    })
-  end,
+	"seblyng/roslyn.nvim",
+	ft = "cs",
+	---@module 'roslyn.config'
+	---@type RoslynNvimConfig
+	opts = {
+		-- your configuration comes here; leave empty for default settings
+	},
 }
